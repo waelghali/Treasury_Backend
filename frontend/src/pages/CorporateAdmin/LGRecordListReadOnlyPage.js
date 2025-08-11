a73c8cede74f76629d53a7c8c43b0c2830888499
Wael Ghali <waelghali79@gmail.com>
2025-08-11 15:08:35 +0300

    Remove frontend from backend repo; moved to separate repo

--- frontend/src/pages/CorporateAdmin/LGRecordListReadOnlyPage.js
+++
@@ -1,9 +0,0 @@
-// frontend/src/pages/CorporateAdmin/LGRecordListReadOnlyPage.js
-import React from 'react';
-import LGRecordList from '../EndUser/LGRecordList';
-
-function LGRecordListReadOnlyPage({ isGracePeriod }) {
-    return <LGRecordList isCorporateAdminView={true} isGracePeriod={isGracePeriod} />;
-}
-
-export default LGRecordListReadOnlyPage;
